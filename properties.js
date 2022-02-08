b7a1661f555fb462539b2763089eba4323b76178
arafat356 <ahmedarafatft@gmail.com>
2022-02-09 02:56:06 +0600

    key

---
+++ properties.js
@@ -0,0 +1,6 @@
+const buttle = { color: 'yellow', hold: 'water', price: 50, isCleaned: true };
+//1 getting all property names
+const keys = Object.keys(buttle);
+// console.log(keys);
+const values = Object.values(buttle);
+console.log(values);

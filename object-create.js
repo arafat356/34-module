79c670e2d0ff1200ed5a407c09f81f7b8385b6d8
arafat356 <ahmedarafatft@gmail.com>
2022-02-08 21:38:57 +0600

    second commit

--- object-create.js
+++ object-create.js
@@ -1,13 +1,13 @@
-// using object literal 
+//1 using object literal 
 const student = {name: 'sakib al hasan',job:'cricketer'};
-// construtor
+//2 construtor
 const person = new Object();
 // console.log(person)
-// another one 
+//3 another one 
 // const human = Object.create(null)
 const human = Object.create(student)
 // console.log(human.name)
-
+//4 class 
 class People {
          constructor(name, age){
                   this.name = name;
@@ -17,7 +17,7 @@ class People {
 const Peop = new People('Manus',12);
 console.log(Peop)
 
-// function 
+//5 function 
 function Manus(name){
          this.name = name;
 }
